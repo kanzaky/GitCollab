a87568819134fb5637057e30c51fb10d2e4c94cc
kanzaky <kanzakycloud@gmail.com>
2022-03-20 22:28:57 +0000

    Classe PortaContentores

---
+++ src/PortaContentores.java
@@ -0,0 +1,12 @@
+public class PortaContentores extends Navio{
+    private int XContentores;
+    public PortaContentores(String registration) {
+        super(registration);
+    }
+    public int getXContentores() {
+        return XContentores;
+    }
+    public void setXContentores(int XContentores) {
+        this.XContentores = XContentores;
+    }
+}
