65316160d62ec37201fcd8fe42bbff847d704466
kanzaky <kanzakycloud@gmail.com>
2022-03-20 22:56:52 +0000

    Classe Porto Classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Porto porto = new Porto(2);
+
+        Petroleiro petro1 = new Petroleiro("X89HB"); petro1.setCargo(250);
+        PortaContentores pConta1 = new PortaContentores("P9H78"); pConta1.setXContentores(100);
+        Navio n = new Navio("H88Jk");
+
+        porto.newNavio(petro1);
+        porto.newNavio(pConta1);
+        porto.newNavio(n);
+
+        System.out.println("Capacidade total = " + porto.getTotalCapacity() + " toneladas.");
     }
 }
---
+++ src/Porto.java
@@ -0,0 +1,48 @@
+public class Porto {
+    private int qntNavios;
+    private int MaxNavios;
+    private Navio[] navios;
+
+    public Porto(int MaxNavios) {
+        this.MaxNavios = MaxNavios;
+        navios = new Navio[MaxNavios];
+        qntNavios = 0;
+    }
+
+    public Navio[] getNavios() {
+        return navios;
+    }
+
+    public int getMaxNavios() {
+        return MaxNavios;
+    }
+
+    public void newNavio(Navio x){
+        if(qntNavios == MaxNavios) {
+            System.out.println("No máximo de navios " + MaxNavios + "");
+            return;
+        }
+        boolean find = false;
+        for(int i=0;i<qntNavios;i++){
+            if(x.getRegistration().equals(navios[i].getRegistration())) {
+                find = true;
+                System.out.println("Matrcula existe: " + x.getRegistration());
+            }
+        }
+        if (!find) {
+            navios[qntNavios] = x;
+            qntNavios++;
+        }
+    }
+
+    public float getTotalCapacity(){
+        float total = 0;
+        for(int i=0; i<qntNavios; i++){
+            if( navios[i] instanceof Petroleiro)
+                total += ((Petroleiro) navios[i]).getCargo();
+            if( navios[i] instanceof PortaContentores)
+                total += ((PortaContentores) navios[i]).getXContentores() * 10;
+        }
+        return total;
+    }
+}
