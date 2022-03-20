0ac91fdcb384593f7e9d963d17066a3423e82dfa
kanzaky <kanzakycloud@gmail.com>
2022-03-20 22:23:00 +0000

    Classe Navio

---
+++ src/Navio.java
@@ -0,0 +1,24 @@
+public class Navio {
+    private String registration;
+    private String name;
+    private float size;
+
+    public Navio(String matricula) {
+        this.registration = matricula;
+    }
+    public String getRegistration() {
+        return registration;
+    }
+    public String getName() {
+        return name;
+    }
+    public float getComprimento() {
+        return size;
+    }
+    public void setName(String nome) {
+        this.name = nome;
+    }
+    public void setSize(float size) {
+        this.size = size;
+    }
+}
