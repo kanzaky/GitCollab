55b662eb9b2b18a92269ae6d2a1940179b248d41
kanzaky <kanzakycloud@gmail.com>
2022-03-20 22:32:08 +0000

    Classe Petroleiro

---
+++ src/Petroleiro.java
@@ -0,0 +1,12 @@
+public class Petroleiro extends Navio{
+    private float cargo;
+    public Petroleiro(String registration) {
+        super(registration);
+    }
+    public float getCargo() {
+        return cargo;
+    }
+    public void setCargo(float cargo) {
+        this.cargo = cargo;
+    }
+}
